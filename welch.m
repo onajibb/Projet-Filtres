b3d6054bad48509342728e16e0306feb4eeffce7
onajibb <71511227+onajibb@users.noreply.github.com>
2022-03-13 14:12:02 +0100

    Update welch.m

--- welch.m
+++ welch.m
@@ -15,5 +15,40 @@ function [f,DSP] = welch(pourcentage_rec, fenetre, signal, N, NFFT,fmin,fmax,fec
     end
 
     DSP = mean(segments); %moyenne
+    %% La DSP sur les periodes demandées
+    
+    % Frequence d'interet 
+    fmin=round(fmin/(fech/NFFT))*(fech/NFFT);
+    fmax=round(fmax/(fech/NFFT))*(fech/NFFT);
+    f = fmin : fech/NFFT : fmax-fech/NFFT;
+    plage=fmax-fmin;
+    % nombre de points d'interet
+    Nplage=((plage)/fech)*NFFT;
+    fmin=mod(fmin+fech/2,fech)-fech/2;
+    FinPeriode=mod(((fech/2-fmin)/fech)*NFFT,NFFT);
+    Periode=NFFT;
+
+    if(Nplage<=FinPeriode)
+        Pi=((fmin+fech/2)/fech)*NFFT/Periode;
+        Pf=((fmin+plage+fech/2)/fech)*NFFT/Periode;       
+        s=DSP(1+round(Pi*end):round(Pf*end));
+    
+    elseif(Nplage>FinPeriode)
+        
+        P=((fmin+fech/2)/fech)*NFFT/Periode;
+        s=DSP(1+round(P*end):end);
+        Nplage=Nplage-((fech/2-fmin)/fech)*NFFT;
+        
+        while(Nplage>Periode)
+            s=[s DSP];
+            Nplage=Nplage-Periode;
+        end
+        PF=Nplage/Periode;
+        s=[s DSP(1:round(PF*end))];
+        
+    end
+
+
+    DSP=s;
 
 end
